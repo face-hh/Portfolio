f69fa4f95d7dc74e138f9d771b840fc201cabe3c
Face <xxfacexc@gmail.com>
2022-02-25 09:30:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use("/img", express.static("img"))
 app.use("/static", express.static("static"))
 
 app.get("/", (_req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "/index.html"))
 })
 
-console.log(__dirname, "index.html")
+console.log(__dirname, "/index.html")
 app.listen(5000, () => console.log(`Live At http://facedev.xyz`));
