c5fae32e6cd296fa91408bda0f8583b19b92b01c
Face <xxfacexc@gmail.com>
2022-02-27 09:58:19 +0200

    added youtube

--- static/js/main.js
+++ static/js/main.js
@@ -18,11 +18,16 @@ document.getElementById('paypal').addEventListener('click', function () {
     window.open('https://paypal.me/FaceDev', '_blank').focus();
 });
 
+document.getElementById('youtube').addEventListener('click', function () {
+    window.open('https://youtube.com/c/FaceDevStuff', '_blank').focus();
+});
+
 setInterval(update, 10000);
 
 function update() {
 
     tippy('#discord', { delay: 100, theme: 'discord', content: 'My Discord server', });
+    tippy('#youtube', { delay: 100, theme: 'youtube', content: 'My YouTube channel', });
     tippy('#github', { delay: 100, theme: 'github', content: 'My GitHub profile', });
     tippy('#paypal', { delay: 100, theme: 'paypal', content: 'My PayPal link for donations' });
     tippy('.logo', { delay: 100, placement: 'right', content: 'Damn you really like to hover everywhere!', });
