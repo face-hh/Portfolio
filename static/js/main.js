be4fa3c9b550ac387ac0819310f8169840533f3b
Face <xxfacexc@gmail.com>
2022-04-27 21:30:21 +0300

    update

--- static/js/main.js
+++ static/js/main.js
@@ -21,16 +21,3 @@ document.getElementById('paypal').addEventListener('click', function () {
 document.getElementById('youtube').addEventListener('click', function () {
     window.open('https://youtube.com/c/FaceDevStuff', '_blank').focus();
 });
-
-setInterval(update, 10000);
-
-function update() {
-
-    tippy('#discord', { delay: 100, theme: 'discord', content: 'My Discord server', });
-    tippy('#youtube', { delay: 100, theme: 'youtube', content: 'My YouTube channel', });
-    tippy('#github', { delay: 100, theme: 'github', content: 'My GitHub profile', });
-    tippy('#paypal', { delay: 100, theme: 'paypal', content: 'My PayPal link for donations' });
-    tippy('.logo', { delay: 100, placement: 'right', content: 'Damn you really like to hover everywhere!', });
-    tippy('.name', { delay: 100, placement: 'left', content: 'AAAAAAAAA', });
-    tippy('.xd', { delay: 100, placement: 'bottom', content: 'DM me if you\'re interested in hiring me!'})
-}
